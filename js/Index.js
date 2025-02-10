20608523d165003abc745ac15c71178b9745ec33
Vdeilae <59042223+Vdeilae@users.noreply.github.com>
2025-02-10 11:59:19 +0800

    Update Index.js

--- js/Index.js
+++ js/Index.js
@@ -56,7 +56,7 @@ document.getElementById('lang-cn').addEventListener('click', () => {
     document.getElementById('email').textContent = '邮箱: vdeilae@qq.com';
     document.getElementById('tools-title').textContent = '实用工具官网链接';
     document.getElementById('maintenance-title').textContent = '网站维护';
-    document.getElementById('maintenance-text').innerHTML = '网站维护不易，赞助点吧，一分两分都是爱！付款时记得备注用于VDLand网站维护。<a href="./sponsor.html">赞助链接(点此跳转)</a>';
+    document.getElementById('maintenance-text').innerHTML = '网站维护不易，赞助点吧，一分两分都是爱！付款时记得备注用于VFUN官网维护。<a href="./sponsor.html">赞助链接(点此跳转)</a>';
     document.getElementById('christmas-title').textContent = '圣诞树';
     document.getElementById('christmas-text').innerHTML = '<a href="./christmas.html">圣诞树(点此跳转)</a>';
 
@@ -74,7 +74,7 @@ document.getElementById('lang-en').addEventListener('click', () => {
     document.getElementById('email').textContent = 'Email: vdeilae@qq.com';
     document.getElementById('tools-title').textContent = 'Useful Tools Official Links';
     document.getElementById('maintenance-title').textContent = 'Website Maintenance';
-    document.getElementById('maintenance-text').innerHTML = 'Website maintenance is not easy, please sponsor a bit, every penny counts! Remember to note that it is for VDLand website maintenance when paying. <a href="./sponsor.html">Sponsor Link (Click to jump)</a>';
+    document.getElementById('maintenance-text').innerHTML = 'Website maintenance is not easy, please sponsor a bit, every penny counts! Remember to note that it is for VFUN website maintenance when paying. <a href="./sponsor.html">Sponsor Link (Click to jump)</a>';
     document.getElementById('christmas-title').textContent = 'Christmas Tree';
     document.getElementById('christmas-text').innerHTML = '<a href="./christmas.html">Christmas Tree (Click to jump)</a>';
     document.querySelector('.footer').textContent = '&copy; 2023 Vdeilae. All rights reserved. ';
